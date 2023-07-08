441df86229e2b800a9078a59f0ede292f4edb996
Camerooooon <cjpuffnstuff@gmail.com>
2023-07-08 14:03:57 -0700

    Change error message

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn rocket() -> _ {
     let database_url = "mysql://cameron:pass@127.0.0.1/photoapp";
     let pool = database::connect_database(database_url)
         .await
-        .expect("Unable to connect to the database, the server could not be started!");
+        .expect("Unable to connect to the database");
     database::initalise_database(&pool)
         .await
         .expect("Failed to initalise database");
